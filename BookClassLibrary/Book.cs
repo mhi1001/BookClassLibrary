37fa9d6697211741bb8846e8e09eb014b14342f8
pc <pc>
2021-10-05 19:52:33 +0200

    Add project files.

---
+++ BookClassLibrary/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookClassLibrary
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
